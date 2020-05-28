bda5673887b5ba18ee066211aa9201c9b36dbd87
githubjonny <jon@ourmessagebox.com>
2020-05-28 19:34:33 +0100

    Update version.php

--- webguides/version.php
+++ webguides/version.php
@@ -1,7 +1,8 @@
 <?php 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020052501;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2020052500;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015051109;        // Requires this Moodle version.
 $plugin->component = 'local_webguides'; // Full name of the plugin (used for diagnostics).
+$plugin->maturity = MATURITY_BETA;
 ?>
